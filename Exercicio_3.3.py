8a23557bf9885740c40bc8fbf36749db8e4d33c8
Mr Bonnano <lucasbonnano@gmail.com>
2019-10-31 22:58:34 -0300

    Refazendo Exercicio 3.4

---
+++ Exercicio_3.3.py
@@ -0,0 +1,37 @@
+print("Exercicio Expressao 3.3")
+
+a = True
+b = False
+c = True
+soma = a and a
+soma2 = b and b
+soma3 = not c
+soma4 = not b
+soma5 = not a
+soma6 = a and b
+soma7 = b and c
+soma8 = a or c
+soma9 = b or c
+soma10 = c or a
+soma11 = c or b
+soma12 = c or c
+soma13 = b or b
+
+print("""a = True\nb = False\nc = True""")
+resultado = "Verdadeiro ou Falso: "
+print(resultado, soma, ": ","a and a")
+print(resultado, soma2, ": ", "b and b")
+print(resultado, soma3, ": ", "not c")
+print(resultado, soma4, ": ", "not b")
+print(resultado, soma5, ": ", "not a")
+print(resultado, soma6, ": ", "a and b")
+print(resultado, soma7, ": ", "b and c")
+print(resultado, soma8, ": ", "a or c")
+print(resultado, soma9, ": ", "b or c")
+print(resultado, soma10, ": ", "c or a")
+print(resultado, soma11, ": ", "c or b")
+print(resultado, soma12, ": ", "c or c")
+print(resultado, soma13, ": ", "b or b")
+
+
+
---
+++ Exercicios 3/Exercicio_3.4.py
@@ -0,0 +1,22 @@
+
+print(' Exercicio escreva uma expressão para determinar se uma pessoadeve ou nao pagar imposto. considere que pagar imposto pessoas cujo salario e maior que 1200')
+salario = 1200
+idade = 18
+aprovado = True
+reprovado = False
+
+
+meusalario = int(input("Digite seu Salario: "))
+minhaidade = int(input("Qual Sua idade: "))
+
+
+soma = print(int(meusalario > salario and minhaidade > idade ))
+
+
+if :
+	print('Aprovado!')
+else:
+	print('Reprovado!')
+
+
+
